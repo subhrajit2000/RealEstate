64256042f0bdde72b96c40770c1ea1b014aa6b32
subhrajit2000 <nayaksubhrajit84@gmail.com>
2024-03-19 10:46:49 +0530

    added listings data and realtors but realtor data is not displayed

--- Urbanacre/views.py
+++ Urbanacre/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from .models import Property
-# Create your views here.
+from .models import Property, Realtor
+
 def index(request):
-    prop = Property.objects.first()
-    return render(request, "index.html", {'prop':prop})
+    option = request.GET.get('option', 'featured')  # Get the option from the request URL
+    if option == 'featured':
+        properties = Property.objects.order_by('?')[:6]  # Retrieve random properties for featured section
+    elif option == 'sale':
+        properties = Property.objects.filter(sale_type='For Sale').order_by('?')[:6]  # Retrieve random properties for sale
+    elif option == 'rent':
+        properties = Property.objects.filter(sale_type='For Rent').order_by('?')[:6]  # Retrieve random properties for rent
+    else:
+        properties = Property.objects.order_by('?')[:6]  # Default to featured properties
+    return render(request, "index.html", {'properties': properties})
+
+
+def realtor(request):
+    realtors = Realtor.objects.all().order_by('name')  # Get all realtors sorted by name
+    context = {'realtors': realtors}
+    return render(request, "index.html", context)
